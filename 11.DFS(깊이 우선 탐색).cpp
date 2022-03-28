9a2b067a589f0ef6b646454aa7c8d1b46282ebb8
burperring <91236428+burperring@users.noreply.github.com>
2022-03-29 00:17:15 +0900

    Create 11.DFS(깊이 우선 탐색).cpp

---
+++ 11.DFS(깊이 우선 탐색).cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int number = 7;
+int c[8];
+vector<int> a[8];
+
+void dfs(int x) {
+	if(c[x]) return;
+	c[x] = true;
+	cout << x << ' ';
+	for(int i = 0; i < a[x].size(); i++) {
+		int y = a[x][i];
+		dfs(y);
+	}
+}
+
+int main(void) {
+	// 1과 2을 연결합니다. 
+	a[1].push_back(2);
+	a[2].push_back(1);
+	// 1과 3를 연결합니다.
+	a[1].push_back(3);
+	a[3].push_back(1);
+	// 2과 3를 연결합니다.
+	a[2].push_back(3);
+	a[3].push_back(2);
+	// 2과 4을 연결합니다. 
+	a[2].push_back(4);
+	a[4].push_back(2);
+	// 2과 5를 연결합니다.
+	a[2].push_back(5);
+	a[5].push_back(2);
+	// 3와 6를 연결합니다.
+	a[3].push_back(6);
+	a[6].push_back(3);
+	// 3와 7을 연결합니다.
+	a[3].push_back(7);
+	a[7].push_back(3);
+	// 4와 5를 연결합니다.
+	a[4].push_back(5);
+	a[5].push_back(4);
+	// 6과 7을 연결합니다.
+	a[6].push_back(7);
+	a[7].push_back(6); 
+	// DFS를 수행합니다.
+	dfs(1); 
+	return 0;
+}
